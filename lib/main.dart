d3806cc760fc67fb4fa00c57407ac00e632049ec
FlutLab.io <>
2022-08-13 19:45:21 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,13 @@ void main() {
           title: Text('IAm Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
-        body: Image(
+        body: Center(
+          child: Image(
           image: NetworkImage(
               'https://admin.reformedwiki.com/wp-content/uploads/2020/03/graven-images-scaled.jpg'),
-        ),
+        ), 
       ),
     ),
+  ),
   );
 }
